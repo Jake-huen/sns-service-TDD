3ffed1daf6d09cfda3dc195b2382134eeba92df9
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-06-10 21:35:23 +0900

    FEAT 알림 기능 구현

--- src/main/java/com/heon/sns/controller/response/AlarmResponse.java
+++ src/main/java/com/heon/sns/controller/response/AlarmResponse.java
@@ -6,10 +6,14 @@ import com.heon.sns.model.AlarmType;
 import com.heon.sns.model.User;
 import com.heon.sns.model.entity.AlarmEntity;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AlarmResponse {
 
     private Integer id;
--- src/main/java/com/heon/sns/model/AlarmArgs.java
+++ src/main/java/com/heon/sns/model/AlarmArgs.java
@@ -2,11 +2,13 @@ package com.heon.sns.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AlarmArgs {
     // 알람을 발생시킨 사람
     private Integer fromUserId;
