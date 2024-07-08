51be0e32fa9c767849c445e9af4b05803f28efae
Jake-huen <57055730+Jake-huen@users.noreply.github.com>
2024-07-08 14:52:18 +0900

    Refactoring : Delete JPA -> queryDSL

--- src/main/java/com/heon/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/heon/sns/repository/CommentEntityRepository.java
@@ -5,9 +5,17 @@ import com.heon.sns.model.entity.PostEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Integer> {
     Page<CommentEntity> findAllByPost(PostEntity post, Pageable pageable);
 
-    void deleteAllByPost(PostEntity postEntity);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE CommentEntity entity where entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity);
 }
--- src/main/java/com/heon/sns/repository/LikeEntityRepository.java
+++ src/main/java/com/heon/sns/repository/LikeEntityRepository.java
@@ -4,8 +4,10 @@ import com.heon.sns.model.entity.LikeEntity;
 import com.heon.sns.model.entity.PostEntity;
 import com.heon.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +23,8 @@ public interface LikeEntityRepository extends JpaRepository<LikeEntity, Integer>
 
     List<LikeEntity> findAllByPost(PostEntity post);
 
-    void deleteAllByPost(PostEntity postEntity);
+    @Transactional
+    @Modifying
+    @Query("DELETE LikeEntity entity WHERE entity.post = :post")
+    void deleteAllByPost(@Param("post") PostEntity postEntity); // JPA에서 제공하는 delete는 데이터를 가지고 온 다음 삭제하기 때문에 비효율적이다.
 }
